041237220bdb8a8d1ac9700819ac4ff915541a80
Doxy4 <93224478+Doxy4@users.noreply.github.com>
2024-02-02 18:22:42 +0200

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,3 +1,10 @@
 Config = {}
 
 Config.Command = "marketplace" -- do /marketplace to open menu
+
+Config.CheckForItem = false -- if you want to have it check for item to open
+Config.CheckItem = "phone" -- your item you have to have to be able to open market
+
+function Config.HasItem(items, amount)
+        return QBCore.Functions.HasItem(items, amount)
+end
